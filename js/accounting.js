46bbbeb80e906367e289466100e373e676bcc7a6
Nasim Reza <nasim8382@gmail.com>
2022-02-17 05:10:03 +0600

    remaining balance error message added

--- js/accounting.js
+++ js/accounting.js
@@ -2,8 +2,21 @@
 function getInputValue (amount) {
     const getInput = document.getElementById(amount + '-input').value;
     const convertInputIntoFloat = parseFloat(getInput);
-    return convertInputIntoFloat;
+    if (getInput < 0) {
+        document.getElementById('negative-error-text').style.display = 'block';
+    }
+    // else if (convertInputIntoFloat != 'number') {
+    //     alert('Please...... input number!!');
+    // }
+    else{
+        return convertInputIntoFloat;
+    }
 }
+
+// cross-mark click handler
+document.getElementById('cross-mark').addEventListener('click', function () {
+    document.getElementById('negative-error-text').style.display = 'none';
+})
 
 // total amount function
 function getTotalAmount () {
@@ -17,11 +30,18 @@ function getTotalAmount () {
     const totalExpensesAmount =  foodInput + rentInput + clothesInput;
     getTotalExpenses.innerText = totalExpensesAmount;
 
-    // total amount
-    const getTotal = document.getElementById('total');
-    const totalAmount = incomeInput - totalExpensesAmount;
-    getTotal.innerText = totalAmount;
-    return totalAmount;
+    if (totalExpensesAmount > incomeInput) {
+        document.getElementById('total-balance-error-text').style.display = 'block';
+    }
+
+    else{
+        // total amount
+        const getTotal = document.getElementById('total');
+        const totalAmount = incomeInput - totalExpensesAmount;
+        getTotal.innerText = totalAmount;
+        document.getElementById('total-balance-error-text').style.display = 'none';
+        return totalAmount;
+    }
 }
 
 // calculate button event handler
@@ -39,8 +59,15 @@ document.getElementById('save-button').addEventListener('click', function () {
     const savingAmount = incomeInput /100 * saveInput;
     getSavingAmount.innerText = savingAmount;
 
-    // remaining balance
-    const getRemainingBalance = document.getElementById('remaining-balance');
-    const remainingBalance = getTotalAmount() - savingAmount;
-    getRemainingBalance.innerText = remainingBalance;
+    if ( savingAmount > getTotalAmount() ) {
+        document.getElementById('saving-error-text').style.display = 'block';
+    }
+
+    else {
+        // remaining balance
+        const getRemainingBalance = document.getElementById('remaining-balance');
+        const remainingBalance = getTotalAmount() - savingAmount;
+        getRemainingBalance.innerText = remainingBalance;
+        document.getElementById('saving-error-text').style.display = 'none';
+    }
 })
